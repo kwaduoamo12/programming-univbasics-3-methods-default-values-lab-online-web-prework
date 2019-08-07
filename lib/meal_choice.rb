1ffdb2581d74222504afccff2097ea610fccc4b4
Samuel Owusu <kwadwoamo10@gmail.com>
2019-08-07 20:31:11 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -2,8 +2,6 @@ def method meal_choice
   puts "What a nutritious meal!"
   puts "A plate of #{protein} with #{veg1} and #{veg2}
   puts meal_choice #{veg1}
-end 
   put meal_choice #{veg2}
-end
 puts A plate of #{protein}"
-end
+
